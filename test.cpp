77572c0dcc9f0f7e28e14fbde09eaa92e87ae85e
Kumar Gaurav <153125396+iamgaurva@users.noreply.github.com>
2026-05-25 12:24:43 +0530

    Implement time conversion from hours to minutes and seconds
    Add calculations for minutes and seconds from hours

--- test.cpp
+++ test.cpp
@@ -10,3 +10,20 @@ int main() {
     }
     return 0;
 }
+//
+//
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+    int hour = 5;
+
+    int minutes = hour * 60;
+    int seconds = hour * 60 * 60;
+
+    cout << minutes << endl;
+    cout << seconds << endl;
+
+    return 0;
+}
