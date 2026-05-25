05469992b9c4d96edef053df33446168993b6dd2
Kumar Gaurav <153125396+iamgaurva@users.noreply.github.com>
2026-05-25 12:46:44 +0530

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -27,3 +27,74 @@ int main() {
 
     return 0;
 }
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string word1 = "Naruto";
+    string word2 = "Sasuke";
+
+    int totalLength = word1.length() + word2.length();
+
+    cout << totalLength;
+
+    return 0;
+}
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+    
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int evenSum = 0, oddSum = 0;
+
+    for(int i = 0; i < n; i++) {
+        if(i % 2 == 0)
+            evenSum += arr[i];
+        else
+            oddSum += arr[i];
+    }
+
+    cout << evenSum - oddSum;
+
+    return 0;
+}
+
+
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n) {
+    if(n < 2)
+        return false;
+
+    for(int i = 2; i * i <= n; i++) {
+        if(n % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+int main() {
+    int L, R;
+    cin >> L >> R;
+
+    for(int i = L; i <= R; i++) {
+        if(isPrime(i)) {
+            cout << i << " ";
+        }
+    }
+
+    return 0;
+}
+
+
